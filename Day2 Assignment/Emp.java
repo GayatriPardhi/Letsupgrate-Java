4f835826d8e2f03754a2c90827c32ca48e2b6589
GayatriPardhi <57560918+GayatriPardhi@users.noreply.github.com>
2020-10-11 12:02:40 +0530

    Create Emp.java

---
+++ Day2 Assignment/Emp.java
@@ -0,0 +1,17 @@
+class Employee{  
+String name;
+int age;
+String city;
+}  
+ public class Emp{  
+ public static void main(String args[]){  
+ Employee s1=new  Employee();  
+  s1.age=23;  
+  s1.name="Gayatri";  
+  s1.city="Pune";  
+  System.out.println(" The name is " +s1.name); 
+ System.out.println(" The age is "+s1.age);
+ System.out.println(" The City is "+s1.city);  
+ 
+ }  
+}  
