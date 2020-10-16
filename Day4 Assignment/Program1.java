58ed7632d77e210c63ae0a56704e8b2a7b87e39c
GayatriPardhi <57560918+GayatriPardhi@users.noreply.github.com>
2020-10-16 23:13:35 +0530

    Create Program1.java

---
+++ Day4 Assignment/Program1.java
@@ -0,0 +1,13 @@
+public class Program1 {
+     String name;
+     int age;
+      String power;
+      String weapon;
+       String planet;
+  
+    public static void main(String[] args) {
+     Program1 myObj = new Program1();
+      System.out.println(myObj);
+    }
+  }
+  
